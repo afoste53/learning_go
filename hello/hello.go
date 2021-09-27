183d9717efff5b7de6bfd389a2e127714dc0a249
Gabe <afoste53@gmail.com>
2021-09-27 10:27:27 -0500

    init commit - Hello, World!

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+// package for formating text, logging to console, and more
+import "fmt"
+
+// now we're adding another module?
+import "rsc.io/quote"
+
+
+func main(){
+   fmt.Println(quote.Go())
+}
